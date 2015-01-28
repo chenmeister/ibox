c8c3e4bb72ec8d4f07f698d32b2027f626056441
Sean Chen <sean.chen909@gmail.com>
2015-01-28 14:57:52 -0800

    Unit Tests done and Integration test set up, still working on coverage and stuff

--- src/test/java/edu/csupomona/cs585/ibox/sync/GoogleDriveFileSyncManagerTest.java
+++ src/test/java/edu/csupomona/cs585/ibox/sync/GoogleDriveFileSyncManagerTest.java
@@ -2,57 +2,155 @@ package edu.csupomona.cs585.ibox.sync;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
+//import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.*;
+import org.mockito.Mockito;
 
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import com.google.api.services.drive.Drive.Files;
+import com.google.api.services.drive.Drive.Files.Delete;
+import com.google.api.services.drive.Drive.Files.Insert;
+import com.google.api.services.drive.Drive.Files.List;
+import com.google.api.services.drive.Drive.Files.Update;
+import com.google.api.client.http.AbstractInputStreamContent;
 
 public class GoogleDriveFileSyncManagerTest {
 	
-	private static Drive mockDrive;
-	private static GoogleDriveFileSyncManager fileSync;
-	private File localFile;
-	private Files getFiles;
+	//declare google drive objects
+	Drive mockDrive;
+	GoogleDriveFileSyncManager fileSync;
+	
+	//declare file objects
+	java.io.File localFile;
+	File body;
+	Files getFiles;
+	List getList;
+	FileList listOfFiles;
+	java.util.List<File> filesList;
+	
+	//declare google drive function objects
+	Insert addInstance;
+	Update updateInstance;
+	Delete deleteInstance;
 	
 	@Before
 	public void setUp() throws Exception {
+		//initialize google drive objects
 		mockDrive = Mockito.mock(Drive.class);
-		fileSync = Mockito.mock(GoogleDriveFileSyncManager.class);
-		localFile = Mockito.mock(File.class);
+		fileSync = new GoogleDriveFileSyncManager(mockDrive);
+		
+		//initialize file objects
+		localFile = Mockito.mock(java.io.File.class);
+		body = new File();
+		listOfFiles = new FileList();
 		getFiles = Mockito.mock(Files.class);
+		getList = Mockito.mock(List.class);
+		filesList = new ArrayList<File>();
+		
+		//initialize google drive function objects
+		addInstance = Mockito.mock(Insert.class);
+		updateInstance = Mockito.mock(Update.class);
+		deleteInstance = Mockito.mock(Delete.class);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		//set all objects to null after completion
 		mockDrive = null;
 		fileSync = null;
 		localFile = null;
+		body = null;
+		listOfFiles = null;
 		getFiles = null;
+		getList = null;
+		filesList = null;
+		addInstance = null;
+		updateInstance = null;
+		deleteInstance = null;
+	}
+	
+	public void initalizeFiles(){
+		File testFile = new File();
+		testFile.setId("TestFile");
+		testFile.setTitle("test.txt");
+		
+		filesList.add(testFile);
+		listOfFiles.setItems(filesList);
 	}
 
 	@Test
 	public void testAddFile() throws IOException {
-		getFiles = mockDrive.files();
+		//initalize all the variables
+		Mockito.when(mockDrive.files()).thenReturn(getFiles);
+		Mockito.when(getFiles.insert(Mockito.isA(File.class), 
+				Mockito.isA(AbstractInputStreamContent.class))).thenReturn(addInstance);
+		Mockito.when(addInstance.execute()).thenReturn(body);
+
 		fileSync.addFile(localFile);
-		//assertEquals(getFiles.get(localFile.getName()), localFile.getName());
+		
+		//verify the variables
+		Mockito.verify(mockDrive, Mockito.atLeastOnce()).files();
+	    Mockito.verify(getFiles, Mockito.atLeast(0)).insert(Mockito.isA(File.class));
+	    Mockito.verify(addInstance, Mockito.atLeastOnce()).execute();
 	}
 
 	@Test
 	public void testUpdateFile() throws IOException {
-
-		fileSync.updateFile(localFile);
+		initalizeFiles();
 		
-	}
+		Mockito.when(localFile.getName()).thenReturn("test.txt");
+		Mockito.when(mockDrive.files()).thenReturn(getFiles);
+		Mockito.when(getFiles.list()).thenReturn(getList);
+		Mockito.when(getList.execute()).thenReturn(listOfFiles);
+		
+		String fileId = fileSync.getFileId("test.txt");
+		
+		Mockito.when(getFiles.update(Mockito.eq(fileId),
+				Mockito.isA(File.class), Mockito.isA(AbstractInputStreamContent.class))).thenReturn(updateInstance);
 
+		Mockito.when(updateInstance.execute()).thenReturn(body);
+		
+		fileSync.updateFile(localFile);
+	
+		Mockito.verify(mockDrive, Mockito.atLeastOnce()).files();
+	    Mockito.verify(getFiles, Mockito.atLeast(0)).update(Mockito.eq(fileId), Mockito.isA(File.class));
+	    Mockito.verify(updateInstance, Mockito.atLeastOnce()).execute();
+	}
+	
+	@Test(expected = Exception.class)
+	public void testUpdateFileExecption() throws IOException {
+		Mockito.when(localFile.getName()).thenReturn(null);
+		fileSync.updateFile(localFile);
+		assertNull(localFile);
+	}
+	
+	
 	@Test
 	public void testDeleteFile() throws IOException {
+		
+		initalizeFiles();
+		
+		Mockito.when(localFile.getName()).thenReturn("test.txt");
+		Mockito.when(mockDrive.files()).thenReturn(getFiles);
+		Mockito.when(getFiles.list()).thenReturn(getList);
+		Mockito.when(getList.execute()).thenReturn(listOfFiles);
+		
+		String fileId = fileSync.getFileId("test.txt");
+		
+		Mockito.when(getFiles.delete(Mockito.eq(fileId))).thenReturn(deleteInstance);
+		Mockito.when(deleteInstance.execute()).thenReturn(null);
+		
 		fileSync.deleteFile(localFile);
+		
+		Mockito.verify(mockDrive, Mockito.atLeastOnce()).files();
+	    Mockito.verify(getFiles, Mockito.atLeast(0)).delete(Mockito.eq(fileId));
+	    Mockito.verify(deleteInstance, Mockito.atLeastOnce()).execute();		
 	}
-
 }
---
+++ src/test/java/edu/csupomona/cs585/ibox/sync/IntegrationTest.java
@@ -0,0 +1,62 @@
+package edu.csupomona.cs585.ibox.sync;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+
+public class IntegrationTest {
+
+	Drive googleDriveClient;
+	GoogleDriveFileSyncManager fileSync;
+	
+	@Before
+	public void setUp() throws Exception {
+		initGoogleDriveServices();
+		fileSync = new GoogleDriveFileSyncManager(googleDriveClient);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		fileSync = null;
+	}
+
+	public void initGoogleDriveServices() throws IOException {
+        HttpTransport httpTransport = new NetHttpTransport();
+        JsonFactory jsonFactory = new JacksonFactory();
+
+        try{
+            GoogleCredential credential = new  GoogleCredential.Builder()
+              .setTransport(httpTransport)
+              .setJsonFactory(jsonFactory)
+              .setServiceAccountId("936890365418-r1gns1if70m5fc44i4a6e3oumi8p5vp8@developer.gserviceaccount.com")
+              .setServiceAccountScopes(Collections.singleton(DriveScopes.DRIVE))
+              .setServiceAccountPrivateKeyFromP12File(new java.io.File("My Project-8e678d0d2682.p12"))
+              .build();
+
+            googleDriveClient = new Drive.Builder(httpTransport, jsonFactory, credential).setApplicationName("ibox").build();
+        }catch(GeneralSecurityException e){
+            e.printStackTrace();
+        }
+
+    }
+	@Test
+	public void testAddFile(){
+		
+	}
+
+}
